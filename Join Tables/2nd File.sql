12a0cb30bcc72cca61b4d2983c78cb6e87620978
Kheshwenda Narasimban <khesh555@gmail.com>
2026-04-01 04:04:29 +0800

    Update 2nd File.sql

--- Join Tables/2nd File.sql
+++ Join Tables/2nd File.sql
@@ -1,1 +1,10 @@
-
+SELECT e.department, COUNT(c.campaign_id) AS total_campaigns
+FROM Employee e
+JOIN Client cl ON e.employee_id = cl.employee_id
+JOIN Campaign c ON cl.client_id = c.client_id
+JOIN Management m ON e.employee_id = m.employee_id
+WHERE e.employementStatus = 'Active'
+  AND cl.client_priority_tier IN ('Gold', 'Silver')
+  AND c.version_tag = 'V1'
+  AND m.annual_budget_allocation >= 200000
+GROUP BY e.employee_id, e.department;

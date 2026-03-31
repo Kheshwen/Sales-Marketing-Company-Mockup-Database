0dfafe2c885c5839c7fd9f42954b1920096aefb6
Kheshwenda Narasimban <111118489+Kheshwen@users.noreply.github.com>
2026-04-01 03:40:42 +0800

    Update 3rd File.sql

--- Join Tables/3rd File.sql
+++ Join Tables/3rd File.sql
@@ -1,20 +1,17 @@
-SELECT * FROM (
-    SELECT 
-        e.employee_id, e.department,
-        co.email_address, co.job_title,
-        cl.client_id, cl.company_name,
-        ca.campaign_id, ca.campaign_name,
-        ce.execution_id, ce.curr_execution_status,
-        pr.report_id, pr.roi_percentage
+SELECT 
+    e.employee_id, e.department,
+    co.email_address, co.job_title,
+    cl.client_id, cl.company_name,
+    ca.campaign_id, ca.campaign_name,
+    ce.execution_id, ce.curr_execution_status,
+    pr.report_id, pr.roi_percentage
 FROM Employee e
-    JOIN Contact co ON e.contact_id = co.contact_id
-    JOIN Client cl ON e.employee_id = cl.employee_id
-    JOIN Campaign ca ON cl.client_id = ca.client_id
-    JOIN CampaignExecution ce ON ca.campaign_id = ce.campaign_id
-    JOIN PostCampaignReport pr ON ce.execution_id = pr.execution_id
-    WHERE e.department = 'Management'                    
-      AND co.preferred_comm_channel = 'Email'             
-      AND cl.client_status = 'Active'                     
-      AND pr.goal_achievement IN ('Exceeded', 'Achieved') 
-) 
-WHERE ROWNUM <= 3;
+JOIN Contact co ON e.contact_id = co.contact_id
+JOIN Client cl ON e.employee_id = cl.employee_id
+JOIN Campaign ca ON cl.client_id = ca.client_id
+JOIN CampaignExecution ce ON ca.campaign_id = ce.campaign_id
+JOIN PostCampaignReport pr ON ce.execution_id = pr.execution_id
+WHERE e.department = 'Management'                     
+  AND co.preferred_comm_channel = 'Email'           
+  AND cl.client_status = 'Active'                     
+  AND pr.roi_percentage BETWEEN 230 AND 310;         
